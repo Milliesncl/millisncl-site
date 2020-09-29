7c91e8b77e8150697fd1649e208f611dbe006ef4
Millie Senecal <millie.senecal@gmail.com>
2020-09-29 12:59:23 +0200

    responsive navbar

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,18 @@
-// This is where it all goes :)
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuBurgerToggle = document.querySelector("#navbar");
+
+  const menuBurger = () => {
+    if (menuBurgerToggle.className === "navbar") {
+      menuBurgerToggle.className += " responsive";
+    } else {
+      menuBurgerToggle.className = "navbar";
+    };
+  };
+
+  menuBurgerToggle.addEventListener("click", () => {
+      menuBurger();
+  });
+});
+
+
